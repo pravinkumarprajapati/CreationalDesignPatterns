2d0008bd28caa99b158fee9ca3fe26e87caa1611
Pravin Kumar <kumar.pravinm@hotmail.co.in>
2025-10-21 15:27:06 +0530

    Factory and builder desing

---
+++ src/AbstractFactoryMethod.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Abstraction.src
+{
+    public abstract class PaymentProcessor
+    {
+        public string TransactionId { get; set; }
+        public decimal Amount { get; set; }
+        public PaymentProcessor( decimal amount)
+        {
+            Amount = amount;
+            TransactionId = Guid.NewGuid().ToString();
+        }
+        public void LogTransaction()
+        {
+            Console.WriteLine($"[LOG] Transaction ID: {TransactionId}, Amount: {Amount}");
+        }
+        public void ProcessPayment()
+        {
+
+            ValidationPayment();
+            MakePayment();
+
+            SendReceipt();
+        }
+        protected abstract decimal ValidationPayment();
+        public abstract void MakePayment();
+        public abstract void SendReceipt();
+
+
+    }
+
+    public class PayPalPaymentProcessor : PaymentProcessor
+    {
+        public PayPalPaymentProcessor(decimal amount) : base(amount)
+        {
+        }
+        protected override decimal ValidationPayment()
+        {
+            Console.WriteLine("Validating PayPal payment...");
+            return Amount;
+        }
+        public override void MakePayment()
+        {
+            Console.WriteLine($"Processing PayPal payment of {Amount}...");
+        }
+        public override void SendReceipt()
+        {
+            Console.WriteLine("Sending PayPal payment receipt...");
+        }
+    }
+
+    public class StripePaymentProcessor : PaymentProcessor
+    {
+        public StripePaymentProcessor(decimal amount) : base(amount)
+        {
+        }
+        protected override decimal ValidationPayment()
+        {
+            Console.WriteLine("Validating Stripe payment...");
+            return Amount;
+        }
+        public override void MakePayment()
+        {
+            Console.WriteLine($"Processing Stripe payment of {Amount}...");
+        }
+        public override void SendReceipt()
+        {
+            Console.WriteLine("Sending Stripe payment receipt...");
+        }
+    }
+
+    public class ClientPayment
+    {
+        public void Main()
+        {
+            PaymentProcessor paypalProcessor = new PayPalPaymentProcessor(100.00m);
+            paypalProcessor.ProcessPayment();
+            Console.WriteLine();
+            PaymentProcessor stripeProcessor = new StripePaymentProcessor(200.00m);
+            stripeProcessor.ProcessPayment();
+        }
+    }
+}
---
+++ src/ComputerBuilder.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction.src
+{
+    public interface IComputerBuilder
+    {
+        void SetCPU(string cpu);
+        void SetRAM(string ram);
+        void SetMotherboard(string storage);
+        void SetGraphicsCard(string storage);
+        Computer GetComputer();
+    }
+
+    public class Computer
+    {
+        public string Storage { get; set; }
+        public string CPU { get; set; }
+        public string Motherboard { get; set; }
+        public string GraphicsCard { get; set; }
+        public int RAM { get; set; }
+
+
+        public string DisplayConfiguration()
+        {
+            return $"Computer [CPU={CPU}, RAM={RAM}, Storage={Storage}]";
+        }
+    }
+
+    public class HomeComputerBuilder : IComputerBuilder
+    {
+        private Computer _computer = new Computer();
+        public void SetCPU(string cpu)
+        {
+            _computer.CPU = cpu;
+        }
+        public void SetRAM(string ram)
+        {
+            _computer.RAM = int.Parse(ram);
+        }
+        public void SetMotherboard(string motherboard)
+        {
+            _computer.Motherboard = motherboard;
+        }
+        public void SetGraphicsCard(string graphicsCard)
+        {
+            _computer.GraphicsCard = graphicsCard;
+        }
+        public Computer GetComputer()
+        {
+            return _computer;
+        }
+    }
+
+
+    public class GamingComputerBuilder : IComputerBuilder
+    {
+        private Computer _computer = new Computer();
+        public void SetCPU(string cpu)
+        {
+            _computer.CPU = cpu;
+        }
+        public void SetRAM(string ram)
+        {
+            _computer.RAM = int.Parse(ram);
+        }
+        public void SetMotherboard(string motherboard)
+        {
+            _computer.Motherboard = motherboard;
+        }
+        public void SetGraphicsCard(string graphicsCard)
+        {
+            _computer.GraphicsCard = graphicsCard;
+        }
+        public Computer GetComputer()
+        {
+            return _computer;
+        }
+    }
+
+    public class ComputerDirector
+    {
+        private readonly IComputerBuilder _builder;
+        public ComputerDirector(IComputerBuilder builder)
+        {
+            _builder = builder;
+        }
+        public Computer Construct()
+        {
+            _builder.SetCPU("Intel i7");
+            _builder.SetRAM("16");
+            _builder.SetMotherboard("ASUS ROG STRIX Z490-E");
+            _builder.SetGraphicsCard("NVIDIA GeForce RTX 3080");
+            return _builder.GetComputer();
+        }
+    }
+
+    public class Client
+    {
+        public void Main()
+        {
+            IComputerBuilder homeBuilder = new HomeComputerBuilder();
+            ComputerDirector homeDirector = new ComputerDirector(homeBuilder);
+            Computer homeComputer = homeDirector.Construct();
+            Console.WriteLine("Home Computer Configuration: " + homeComputer.DisplayConfiguration());
+            IComputerBuilder gamingBuilder = new GamingComputerBuilder();
+            ComputerDirector gamingDirector = new ComputerDirector(gamingBuilder);
+            Computer gamingComputer = gamingDirector.Construct();
+            Console.WriteLine("Gaming Computer Configuration: " + gamingComputer.DisplayConfiguration());
+        }
+    }
+}
